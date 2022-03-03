ac0878c2e52a34396ef74abab63b6255ef2055bd
Brayner Cardenas <brayner.cardenas@wiedii.co>
2022-03-03 10:47:56 -0500

    firstcommit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const messages = [
+    "Stiven",
+    "Ana",
+    "Nicolay",
+    "Yesica",
+    "carolina",
+    "Luis",
+    "luz",
+    "Diego",
+    "Laura"
+];
+
+
+const randomMsg = () =>{
+    const message = messages[Math.floor(Math.random()*
+        messages.length)];
+        console.log(message);
+};
+
+module.exports = {randomMsg};
